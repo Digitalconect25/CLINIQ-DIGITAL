adbc8e80eb84e92062eb59d08ac3fab8f61dbf69
ServiEnergy <info.digitalconect@gmail.com>
2026-02-27 00:09:16 +0100

    Update anthropic version in API request headers

--- api/generate.js
+++ api/generate.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
       headers: {
         'Content-Type': 'application/json',
         'x-api-key': apiKey,
-        'anthropic-version': '2024-06-01',
+        'anthropic-version': '2023-06-01',
       },
       body: JSON.stringify(anthropicBody),
     });
